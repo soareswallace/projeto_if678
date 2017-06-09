d7febff0b8a6114da18e99a7e3926a59f6f8de6d
chcp <chcp@cin.ufpe.br>
2017-06-09 16:19:31 -0300

    Remove QT

--- part_2/ftp.py
+++ part_2/ftp.py
@@ -1,52 +1,28 @@
 import sys
 import socket  
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
-from PyQt5.QtGui import QIcon
 import pandas as pd
 
 PORTA = 5252
-class App(QWidget):
- 
-    def __init__(self):
-        super(App, self).__init__()
-        self.title = 'IF678 - FTP'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.initUI()
- 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.show()
-        self.openFileNameDialog()
-        #self.openFileNamesDialog()
-        #self.saveFileDialog()
- 
- 
-    def openFileNameDialog(self):    
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
-        if fileName:
-            return fileName
 
 
 if __name__ == '__main__':
-    data = pd.read_csv("dontOpenPasswordsInside.csv").iloc[:,:].values
+    dataset = pd.read_csv("dontOpenPasswordsInside.csv")
+    logins = np.array(dataset.iloc[:,0].values)
+    senhas = dataset.iloc[:,1].values
+
+
     print "Deseja Fazer um Upload(0) ou Download(1) ?"
     operation = int(raw_input())
+    s = socket.socket()         # Create a socket object
+    host = socket.gethostname()  # Get local machine name
+    port = PORTA                 # Reserve a port for your service.
+    print "O host escolhido foi:",host,":",port    
     if operation == 0:  #Upload
-        s = socket.socket()         # Create a socket object
-        host = socket.gethostname() # Get local machine name
-        port = PORTA                 # Reserve a port for your service.
-        print "O host escolhido foi:",host,":",port
         s.connect((host, port))
         print "Escolha o arquivo:"
-        app = QApplication(sys.argv)
-        fileName = App().openFileNameDialog();
+        #Digitar file
+        fileName = raw_input()
         print "O arquivo escolhido foi:",fileName
         
         f = open(fileName,'rb')
@@ -63,10 +39,6 @@ if __name__ == '__main__':
         s.close()
         
     if operation == 1:  #Download
-        s = socket.socket()         # Create a socket object
-        host = socket.gethostname()  # Get local machine name
-        port = PORTA                 # Reserve a port for your service.
-        print "O host escolhido foi:",host,":",port
         s.bind((host, port))        # Bind to the port
         f = open("reciveFile",'wb')
         s.listen(5)                 # Now wait for client connection.
