6aff1511b9ab838ad5bd72df7b4cff4cbb73c1e7
Tancredo <tjssj@cin.ufpe.br>
2017-06-10 22:38:16 -0300

    Server não fecha mais depois que o cliente aperta ctrl + c

--- part_2/ftp_client.py
+++ part_2/ftp_client.py
@@ -7,14 +7,24 @@ PORT = int (sys.argv[1])  #leitura da porta que sera dada com input do user.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criando o socket
 s.connect((HOST, PORT)) # conectando ao servidor
 # USER INTERFACE]
+
+# --------------------- LOGIN -------------------------------------#
 opt = raw_input("\n1 - Criar login\n2 - Login\nDigite opcao: ")
 login = raw_input("Digite seu login: ");
 senha = raw_input("Digite sua senha: ");
 
-s.sendall(opt + "@" +login + "@" + senha) # enviando a string hello world
+s.sendall(opt + "@" + login + "@" + senha) # enviando a string hello world
 data = s.recv(1024) # esperando resposta do servidor
-print 'Received', repr(data) # printa os dados recebidos
+print repr(data)
+#print 'Received', repr(data) # printa os dados recebidos
+#-----------------------------------------------------------------#
+
+#--------------------SISTEMA DE ARQUIVOS--------------------------------#
+while 1:
+    opt = raw_input("Deseja sair? Digite 3: ")
+    if(opt == "3"):
+         break
 
 
-raw_input()
+#--------------------------------------------------------------------------#
 s.close()
--- part_2/ftp_server.py
+++ part_2/ftp_server.py
@@ -8,31 +8,32 @@ PORT = int(sys.argv[1])              # Porta escolhida arbitrariamente escolhida
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cria o Socket
 s.bind((HOST, PORT))
 s.listen(1) # Somente 1 conexao na fila eh aceita
-conn, addr = s.accept() # Aceita uma conexao e guarda o socket que representa a conexao em conn e adress em addr
-print 'Connected by', addr
 
 dataset = np.array( pd.read_csv("dontOpenPasswordsInside.csv").iloc[:,:].values, dtype=str)
 
-while 1:
-    data = conn.recv(1024)
-    if not data: break # Se nao tiver dados recebido (0) significa que a conexao acabou
-    data = data.replace(" ", "")
-    data = data.split('@')
-    option = int(data[0])
-    login = data[1]
-    senha = data[2]
-    #print data         #print de debugger
-    if option == 1:  # Criar
-        if login in dataset[:,0]:
-            conn.sendall("Login ja existente")
-        else:
-            alloc = pd.DataFrame([{login,senha}])
-            alloc.to_csv("dontOpenPasswordsInside.csv", mode="a", header=False, index=False)
+while True:
+    conn, addr = s.accept() # Aceita uma conexao e guarda o socket que representa a conexao em conn e adress em addr
+    print 'Connected by', addr
+    while True:
+        data = conn.recv(1024)
+        if not data: break # Se nao tiver dados recebido (0) significa que a conexao acabou
+        data = data.replace(" ", "")
+        data = data.split('@')
+        option = int(data[0])
+        login = data[1]
+        senha = data[2]
+        #print data         #print de debugger
+        if option == 1:  # Criar
+            if login in dataset[:,0]:
+                conn.sendall("Login ja existente")
+            else:
+                alloc = pd.DataFrame([{login,senha}])
+                alloc.to_csv("dontOpenPasswordsInside.csv", mode="a", header=False, index=False)
+                conn.sendall("Login criado com sucesso!")
+        elif option == 2:
+            if [login,senha] in dataset:
+                conn.sendall("Logado!")
+            else:
+                conn.sendall("Login ou senha incorretos!")
 
-    elif option == 2:
-        if [login,senha] in dataset:
-            conn.sendall("Logado!")
-        else:
-            conn.sendall("Login ou senha incorretos!")
-
-conn.close() # Fecha a conexao
+    conn.close() # Fecha a conexao
