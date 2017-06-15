7f77385efd4661e528dbdbddc97f3a3fe503cde3
Tancredo <tjssj@cin.ufpe.br>
2017-06-15 10:26:52 -0300

    Modularizing code

---
+++ part_2/ftp_file_client.py
@@ -0,0 +1,84 @@
+import socket
+import sys
+
+def init():
+	HOST = "localhost"
+	PORT = int(sys.argv[1])    #leitura da porta que sera dada com input do user.
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criando o socket
+	s.connect((HOST, PORT)) # conectando ao servidor
+	return [HOST, PORT, s]
+
+def readCred():
+	login = raw_input("Digite seu login: ")
+	senha = raw_input("Digite sua senha: ")
+	return [login, senha]
+
+def menu():
+	opt = raw_input("\n1 - Criar login\n2 - Login\n3-Sair\nDigite opcao: ")
+	[login, senha] = readCred()
+	return [opt, login, senha]
+
+def send_string(str):
+	s.sendall(str) # enviando a string hello world
+	data = s.recv(1024) # esperando resposta do servidor
+	print (repr(data))
+	return repr(data)
+
+def conn_interface(opt, login, senha):
+	# Create login
+	if opt == 1:
+		while True:
+			str2send = opt + "@" + login + "@" + senha
+			data = send_string(str2send)
+			if (data != "Login ja existente"):
+				break
+			else:
+				readCred()
+
+	# Login to existing accounts
+	return
+
+#--Global variables-#
+opt = '0'
+data = "l"
+#------------------#
+
+while True:
+	# --------------------------- CONNECTION ----------------------------------#
+	[HOST, PORT, s] = init() # initialize
+
+	[opt, login, senha] = menu() # login (menu)
+	if opt == "3":
+		break
+
+	###########CONNECTION INTERFACE##############
+	conn_interface(opt, login, senha)
+	############################################
+
+	#########          Login        ###########
+	while True:
+		print "Bem-vindo a Zuera"
+		print "Escolha o arquivo"
+		fileName = raw_input()
+		print "O arquivo escolhido foi", fileName
+		f = open(fileName, 'rb')
+		print "Sending..."
+		l = f.read(1024)
+		while (l):
+			print "Sending..."
+			s.send(l)
+			l = f.read(1024)
+		f.close()
+		s.shutdown(socket.SHUT_WR) #esse negocio chato
+		print "Done Sending"
+		data = s.recv(1024) # esperando resposta do servidor
+		print "Servidor disse:", data
+	opt = raw_input("Digite 3 para sair, qualquer outra coisa para continuar...\n")
+	#print 'Received', repr(data) # printa os dados recebidos
+	###########################################
+	#-----------------------------------------------------------------#
+
+	#--------------------SISTEMA DE ARQUIVOS--------------------------------#
+
+	#--------------------------------------------------------------------------#
+s.close()
